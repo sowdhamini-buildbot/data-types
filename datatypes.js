20648640956a5492bc59e323eec001010009f212
chodavarapu sowdhamini <94843433+sowdhamini-buildbot@users.noreply.github.com>
2022-03-08 21:28:52 +0530

    Update datatypes.js

--- datatypes.js
+++ datatypes.js
@@ -7,7 +7,7 @@ Mainly there is 2 kind of data types in Javascript Programming Language
 
 */
 
-//PRIMITIVE TYPES
+//PRIMITIVE TYPES : Primitive data types can store only a single value
 
 // There are 7 kind of Primitive Data Types
 // 01) Number [Integer || 4 && Float || 4.0]
@@ -18,30 +18,37 @@ Mainly there is 2 kind of data types in Javascript Programming Language
 // 06) bigInt
 // 07) Object and symbols
 
-//string -	represents sequence of characters
-let person =" sowdhamini"
+//string -	 It represents a series of characters and is written with quotes. A string can be represented using a single or a double quote.
+let person = "sowdhamini"
 
 
-//number - 	represents numeric values 
+//number - 	It represents a number and can be written with or without decimals.
 let age = 24
+let x=3.6
 
 
-//boolean - represents boolean value either false or true
-const valueChecked= true
+//boolean -  It represents a logical entity and can have only two values : true or false
+var a = 2;
+var b =  3;
+var c =  2;
+(a == b) // returns false
+(a == c) //returns true
 
 
 //null - represents undefined value
 var person = null
 
-//undefined - represents null i.e. no value at all
-var person ;
+//undefined - When a variable is declared but not assigned, it has the value of undefined and it’s type is also undefined.
+var x; // value of x is undefined
+var y = undefined; // we can also set the value of a variable as undefined
+
 
 //symbols -It is a new data type introduced in the ES6 version of javascript. It is used to store an anonymous and unique value.
 var symbol1 = Symbol('symbol');
 
 
 
-//bigint -BigInt type was recently added to the language to represent integers of arbitrary length.A BigInt value is created by appending n to the end of an integer:
+//bigint - This data type is used to store numbers which are above the limitation of the Number data type. It can store large integers and is represented by adding “n” to an integer literal.
 const bigInt = 1234567890123456789012345678901234567890n;
 
 
@@ -51,7 +58,7 @@ let age = 24
 console.log(typeof person)
 
 
-//REFERENCE TYPES
+//REFERENCE TYPES : To store multiple and complex values, non-primitive data types are used.
 // There are 3 data types
 // 01) Arrays
 // 02) objects
